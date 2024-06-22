9fee130774d618acd4bb43b7874345363c5c699d
basedygt <167074291+basedygt@users.noreply.github.com>
2024-06-22 11:10:52 +0000

    Create get_db.py

---
+++ get_db.py
@@ -0,0 +1,12 @@
+import pyrebase
+
+config = {
+  "apiKey": "FIREBASE_API_KEY",
+  "authDomain": "FIREBASE_AUTH_DOMAIN_ID.firebaseapp.com",
+  "databaseURL": "https://FIREBASE_AUTH_DOMAIN_ID.firebaseio.com",
+  "storageBucket": "FIREBASE_AUTH_DOMAIN_ID.appspot.com",
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+print(db.get())
