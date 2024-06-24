cdb3037647e59cdb5671bc5aaa0b4e3a7765bb2b
basedygt <167074291+basedygt@users.noreply.github.com>
2024-06-24 11:45:18 +0000

    Create key_checker.sh

---
+++ key_checker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+  echo -e "\nUsage: $0 <key_to_check>\n"
+  echo -e "Example:"
+  echo -e "  ./key_checker.sh AizxsadnjdasdS_wdccxzsadcc"
+  exit 0
+fi
+
+check_key() {
+  local api_key="$1"
+  local data={"longDynamicLink": "https://sub.example.com/?link=https://example.org"}
+  response=$(curl -X POST "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=$api_key" -H 'Content-Type: application/json' -d $data)
+  if [[ $response != *"API key not valid"* ]]; then
+    echo "[+] Your Firebase api key is valid"
+    exit 0
+  else
+    exit 1
+  fi
+
+check_key "$1"
