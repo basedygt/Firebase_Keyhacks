e7d7bd69af2eb7df0db4d77a3d733b8e03b6a45a
basedygt <167074291+basedygt@users.noreply.github.com>
2024-06-24 12:06:36 +0000

    Update key_checker.sh

--- key_checker.sh
+++ key_checker.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-  echo -e "\nUsage: $0 <key_to_check>\n"
+if [[ $# -ne 1 && $# -ne 2 ]]; then
+  echo -e "\nUsage: $0 <key_to_check> [<file_to_write_valid_key_to>]\n"
   echo -e "Example:"
-  echo -e " ./key_checker.sh AIzaSyBoLkjS3zdlqscfqv2scaB-IOe98QRadsc"
+  echo -e " ./key_checker.sh AIzaSyBoLkjS3zdlqscfqv2scaB-IOe98QRadsc output.txt"
   exit 0
 fi
 
 check_key() {
   local api_key="$1"
+  local output_file="$2"
   local data='{"longDynamicLink": "https://sub.example.com/?link=https://example.org"}'
   response=$(curl -s -X POST "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=$api_key" -H 'Content-Type: application/json' -d "$data")
+  
   if [[ $response != *"API key not valid"* ]]; then
-    echo "[+] Your Firebase api key is valid"
+    echo "[+] Your Firebase API key $api_key is valid"
+    if [[ -n $output_file ]]; then
+      echo "[+] Saved to $output_file"
+      echo "$api_key" > "$output_file"
+    fi
     exit 0
+
   else
-    echo "[+] Your Firebase api key is invalid"
+    echo "[+] Your Firebase API key $api_key is invalid"
     exit 1
   fi
 }
 
-check_key "$1"
+check_key "$1" "$2"
