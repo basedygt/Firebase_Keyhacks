900c4b82aeb220decd4755f948e0748679c895e4
basedygt <167074291+basedygt@users.noreply.github.com>
2024-06-24 11:53:10 +0000

    Update key_checker.sh

--- key_checker.sh
+++ key_checker.sh
@@ -10,12 +10,12 @@ fi
 check_key() {
   local api_key="$1"
   local data='{"longDynamicLink": "https://sub.example.com/?link=https://example.org"}'
-  response=$(curl -X POST "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=$api_key" -H 'Content-Type: application/json' -d "$data")
+  response=$(curl -s -X POST "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=$api_key" -H 'Content-Type: application/json' -d "$data")
   if [[ $response != *"API key not valid"* ]]; then
-    echo "[+] Your Dynamic link Firebase api key is valid"
+    echo "[+] Your Dynamic link Firebase API key is valid"
     exit 0
   else
-    echo "[+] Invalid Key"
+    echo "[+] Your Dynamic link Firebase API key is invalid"
     exit 1
   fi
 }
